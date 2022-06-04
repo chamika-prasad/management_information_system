60a48571cef1daeaa131ce67b131c60965196f53
alokagimhari <alokagimhari@gmail.com>
2022-06-04 22:50:46 +0530

    registerview

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,7 +9,7 @@
                <div class="card">
                    <div class="card-header">Register</div>
                     <div class="card-body">
-                        <form action="{{route('register.post')}}" method="POST">
+                        <form action="{{route('register')}}" method="POST">
                             @csrf
                             <div class="form-group row">
                                 <label for="name" class="col-md-4 col-form-lable text-md-right">Name</label>
