8c1da139ef4b10ff43ccc86aca9de501e9bbc4e2
chamimax <chamikap40@gmail.com>
2022-06-09 01:58:23 +0530

    add admin free learning approve controller

--- app/Http/Controllers/FreeLearningController.php
+++ app/Http/Controllers/FreeLearningController.php
@@ -21,14 +21,26 @@ class FreeLearningController extends Controller
 
     public function displayFreelearningList(){
 
-        $FreeApplications = FreeApplication::all();//get all element in free learning application model
-        return view('payment/admin_free_learning_application_list',['FreeApplications' => $FreeApplications]);
+        $freeApplications = FreeApplication::all();//get all element in free learning application model
+        return view('payment/admin_free_learning_application_list',['freeApplications' => $freeApplications]);
     }
 
     public function adminFreelearning($id){
+        
+        $user_applications = FreeApplication::where('user_id',$id)->get();
+        return view('payment/admin_free_learning_approve',['user_applications' => $user_applications]);
+    }
 
-        $user_applications = FreeApplication::where('user_id', $id)->get();
-        return view('payment/admin_free_learning_approve',compact('user_applications'));
+    public function adminFreeLearningAction($action,$user_id){
+        $freeApplications = FreeApplication::all();//get all element in free learning application model
+        $user_application = FreeApplication::where('user_id',$user_id)->first();
+
+            $user_application->approved = $action;
+           
+            $user_application->save();
+            
+            return view('payment/admin_free_learning_application_list',['freeApplications' => $freeApplications]);
+
     }
 
 }
