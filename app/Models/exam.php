5ff3a6c2ce8eba70bbc98ef9a6300d9fc184e5a6
nimanthaprasad <prasadrmn.19@uom.lk>
2022-06-10 00:27:06 +0530

    exam model create

--- app/Models/exam.php
+++ app/Models/exam.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Exam extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'subject_id',
+        'description_about_exam',
+        'guidline'
+    ];
 }
