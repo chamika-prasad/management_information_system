54eeba760b9a2fd79995f06f310a0b0db08a130d
nimanthaprasad <prasadrmn.19@uom.lk>
2022-06-10 00:28:17 +0530

    change quiz model

--- app/Models/quiz.php
+++ app/Models/quiz.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Quiz extends Model
 {
     use HasFactory;
+    protected $fillable=[
+        'description_about_quiz',
+        'add_quiz_paper',
+        'guidline',
+        'grade',
+        'teacher_id',
+        'subject_id',
+        'date_and_time'
+
+        
+    ];
 }
+
