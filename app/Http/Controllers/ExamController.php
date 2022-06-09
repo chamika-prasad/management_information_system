5cdf349957059188401ff27763df9347a5b80843
nimanthaprasad <prasadrmn.19@uom.lk>
2022-06-10 00:22:17 +0530

    exam controller setup

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Exam;
 
 class ExamController extends Controller
 {
-    //
+    public function addExam(Request $request,$subject_id){
+
+        $exam = new Exam();
+        $exam->subject_id=$subject_id;
+        $exam->description_about_exam=$request->description;
+        $exam->guidline=$request->guidline;
+        $exam->student_id=13;
+        $exam->teacher_id=12;
+        $exam->add_exam_paper='asdfg';
+        $exam->date_and_time='838:59:59';
+        $exam->grade=5;
+        $exam->save();
+        return redirect('/');
+
+    }
 }
