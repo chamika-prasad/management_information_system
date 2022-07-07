916346e1457c8c3ff24a6ac91d0a9adbe3b42f34
alokagimhari <alokagimhari@gmail.com>
2022-07-07 10:11:00 +0530

    add session start

--- resources/views/layouts/Navbar.blade.php
+++ resources/views/layouts/Navbar.blade.php
@@ -18,15 +18,18 @@
                         height="40"
                         alt="Black and White Portrait of a Man"
                         loading="lazy" viewBox="0 0 16 16" style="vertical-align: middle;">
+                        
                           <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                           <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                         </svg>
+                        
                 <div class="dropdown-menu " aria-labelledby="dropdownMenu2">
                   <button class="dropdown-item" type="button"><a href="{{route('my_profile')}}">My Profile</a></button>
                   <button class="dropdown-item" type="button"><a href="{{route('logout')}}">Logout</a></button>
                 </div>
               </div>
               <div>&emsp;</div>
+              
               <span class="navbar-brand mb-0 h1" style="font-weight: bold;"></span>
             </div>
           </nav>
