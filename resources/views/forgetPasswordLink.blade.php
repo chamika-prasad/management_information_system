fbe5049b4cc7b82ad551b564513e4e2408365388
alokagimhari <alokagimhari@gmail.com>
2022-07-07 07:04:58 +0530

    layout change

--- resources/views/forgetPasswordLink.blade.php
+++ resources/views/forgetPasswordLink.blade.php
@@ -11,43 +11,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <title>Free_Learning_Application</title>
 </head>
-<body>
-    @include('layouts.Navbar')
-     <!-- navbar 2-->
-          
-     <nav class="navbar navbar-expand-sm " style="background-color: #7C5D5D;">
-        
-        <!-- Links -->
-        <ul class="navbar-nav">
-    
-            <li class="nav-item">
-                <a class="nav-link" style="link-decoration:none" href="/">
-                
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16" style=" vertical-align: middle; color: black; ">
-                    <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"/>
-                    </svg>
-            
-                    <span  style="font-family: 'Roboto';
-                                    font-style: none;
-                                    font-weight: 400;
-                                    font-size: 24px;
-                                    line-height: 28px;
-                                    color:white;
-                                    "
-                    >
-                        &ensp;Dashboard
-                    </span>
-                </a>
-              
-            </li>
-    
-            <li><div>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</div>
-            </li>
-        
-         
-        </ul>
-      
-      </nav>
 <style>
       input[type=email], input[type=password]{
    width: 100%;
@@ -76,6 +39,13 @@ input[type=email]:focus, input[type=password]:focus {
 }
 
     </style>
+    <body>
+    <nav class="navbar navbar-light " style="background-color: #FDEFEF;">
+            <div class="left-corner">
+                <img src="./images/menu.png" width="35" height="35">
+                <span class="navbar-brand mb-0 h1" style="font-weight: bold;">Welcome To The E-Learning of Dhamma school</span>
+            </div>
+</nav>
 <main class="login-form">
     <div class="container">
         <div class="row justify-content-center">
